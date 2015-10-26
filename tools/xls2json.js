306c900a8dce94e35b8853b533be1aa547c7bb6e
he110world <he110world@126.com>
2015-10-26 20:30:36 +0800

    excel to json

---
+++ tools/xls2json.js
@@ -0,0 +1,106 @@
+var args = process.argv.slice(2);
+var file = args[0];
+if (!file) {
+	console.log('Usage: node xls2json.js <table-file>.xls* --skip <col-to-skip> <col-to-skip>');
+	process.exit();
+}
+
+var skips = [];
+var keeps = [];
+var state = '';
+for (var i=1; i<args.length; i++) {
+	if (args[i] == '--skip' || args[i] == '-s') {
+		state = 'skip';
+		continue;
+	}
+
+	if (args[i] == '--keep' || args[i] == '-k') {
+		state = 'keep';
+		continue;
+	}
+
+	if (state == 'skip') {
+		skips.push(args[i]);
+	} else if (state == 'keep') {
+		keeps.push(args[i]);
+	}
+}
+
+var excel = require('node-xlsx');
+var obj = excel.parse(file);
+var table = {};
+for (var i in obj) {
+	var sheet = obj[i];
+	if (state == 'skip') {
+		if (skips.indexOf(sheet.name) != -1) {
+			continue;
+		}
+	} else if (state == 'keep') {
+		if (keeps.indexOf(sheet.name) == -1) {
+			continue;
+		}
+	}
+
+	if (sheet.name == 'exp') {	// exp table is different
+		// exp structure:
+		// exp : {
+		//		Lv : [...]
+		//		RoleExp : [...]
+		//		GirlExp : [...]
+		// }
+		// line 2 is column name
+		var exp = {};
+		var columns = sheet.data[2];
+		var numrows = sheet.data.length - 3;
+		for (var j in columns) {
+			var col = columns[j];
+			var vals = [0];	// exp ID starts from 1
+			for (var k=0; k<numrows; k++) {
+				var val = sheet.data[k+3][j];
+				if (k>0 && val==0) {	// first data row can contain 0's
+					break;
+				}
+				vals.push(val);
+			}
+			exp[col] = vals;
+		}
+		table.exp = exp;
+	} else {
+		// data structure:
+		// data : {
+		//		ID1 : {
+		//			Name : xxx,
+		//			Rare : xxx
+		//		},
+		//		ID2 : {
+		//			Name : xxx,
+		//			Rare : xxx
+		//		}
+		// }
+		// line 2 is column name
+		// find ID column
+		var colnames = sheet.data[2];
+		var IDcol = 0;
+		for (var j in colnames) {
+			if (colnames[j] == 'ID') {
+				IDCol = j;
+				break;
+			}
+		}
+		var data = {};
+		for (var j=3; j<sheet.data.length; j++) {
+			var ID = sheet.data[j][IDcol];
+			var onedata = {};
+			for (var k in colnames) {
+				var colname = colnames[k];
+				if (colname) {
+					onedata[colname] = sheet.data[j][k];
+				}
+			}
+			data[ID] = onedata;
+		}
+		table[sheet.name] = data;
+	}
+}
+
+console.log(JSON.stringify(table));
